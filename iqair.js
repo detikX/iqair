2c275bafdd5b0e01ef1762fdf15c537157ad637a
dediwibisono17 <dedi.wibisono17@gmail.com>
2024-06-25 13:26:29 +0700

    frf

--- iqair.js
+++ iqair.js
@@ -11,7 +11,7 @@ var bicycleRental = {
         judul: "Kelapa Gading",
         longitude: "-6.1415940555852355",
         latitude: "106.85626616482651",
-        dki: "DKI2",
+        dki: "DKI 2",
         val: "98",
         ket: "Sedang",
         pm: "2,5",
@@ -30,7 +30,7 @@ var bicycleRental = {
         judul: "Bunderan HI",
         longitude: "-6.195053",
         latitude: "106.823161",
-        dki: "DKI1",
+        dki: "DKI 1",
         val: "85",
         ket: "Sedang",
         pm: "2,5",
@@ -49,7 +49,7 @@ var bicycleRental = {
         judul: "Kebon Jeruk",
         longitude: "-6.192137",
         latitude: "106.770446",
-        dki: "DKI5",
+        dki: "DKI 5",
         val: "85",
         ket: "Sedang",
         pm: "2,5",
@@ -68,7 +68,35 @@ var bicycleRental = {
         judul: "Jagakarsa",
         longitude: " -6.336509",
         latitude: "106.818349",
-        dki: "DKI3",
+        dki: "DKI 3",
+        val: "101",
+        ket: "Tidak Sehat",
+        pm: "2,5",
+        warna_sektor: "#00ff00",
+        short_narasi: "<b>Narasi:</b><br>",
+      },
+    },
+
+
+  ],
+};
+
+var jagakarsa = {
+
+  features: [
+
+
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [106.818349, -6.336509],
+      },
+      properties: {
+        judul: "Jagakarsa",
+        longitude: " -6.336509",
+        latitude: "106.818349",
+        dki: "DKI 3",
         val: "101",
         ket: "Tidak Sehat",
         pm: "2,5",
