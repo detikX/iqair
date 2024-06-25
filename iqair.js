968dab42e3f03002636cc578a8cf5f720807f62d
dediwibisono17 <dedi.wibisono17@gmail.com>
2024-06-25 13:21:25 +0700

    update

---
+++ iqair.js
@@ -0,0 +1,82 @@
+var bicycleRental = {
+
+  features: [
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [106.85626616482651, -6.1415940555852355,],
+      },
+      properties: {
+        judul: "Kelapa Gading",
+        longitude: "-6.1415940555852355",
+        latitude: "106.85626616482651",
+        dki: "DKI2",
+        val: "98",
+        ket: "Sedang",
+        pm: "2,5",
+        warna_sektor: "#00ff00",
+        short_narasi: "<b>Narasi:</b><br>",
+      },
+    },
+
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [106.823161, -6.195053],
+      },
+      properties: {
+        judul: "Bunderan HI",
+        longitude: "-6.195053",
+        latitude: "106.823161",
+        dki: "DKI1",
+        val: "85",
+        ket: "Sedang",
+        pm: "2,5",
+        warna_sektor: "#00ff00",
+        short_narasi: "<b>Narasi:</b><br>",
+      },
+    },
+
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [106.770446, -6.192137],
+      },
+      properties: {
+        judul: "Kebon Jeruk",
+        longitude: "-6.192137",
+        latitude: "106.770446",
+        dki: "DKI5",
+        val: "85",
+        ket: "Sedang",
+        pm: "2,5",
+        warna_sektor: "#00ff00",
+        short_narasi: "<b>Narasi:</b><br>",
+      },
+    },
+
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [106.818349, -6.336509],
+      },
+      properties: {
+        judul: "Jagakarsa",
+        longitude: " -6.336509",
+        latitude: "106.818349",
+        dki: "DKI3",
+        val: "101",
+        ket: "Tidak Sehat",
+        pm: "2,5",
+        warna_sektor: "#00ff00",
+        short_narasi: "<b>Narasi:</b><br>",
+      },
+    },
+
+
+  ],
+};
